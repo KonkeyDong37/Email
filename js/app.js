d87a995af28f8b3d440af567902282a88ffb1b01
Andrey <KonkeyDong37@gmail.com>
2019-01-31 11:03:58 +0500

    add: Добавил возможность сброса формы по кнопке

--- js/app.js
+++ js/app.js
@@ -2,7 +2,9 @@
 const sendBtn = document.getElementById('sendBtn'),
       email = document.getElementById('email'),
       subject = document.getElementById('subject'),
-      message = document.getElementById('message');
+      message = document.getElementById('message'),
+      resetBtn = document.getElementById('resetBtn'),
+      sendEmailForm = document.getElementById('email-form');
 
 
 
@@ -18,6 +20,9 @@ function eventListeners() {
     email.addEventListener('blur', validateField);
     subject.addEventListener('blur', validateField);
     message.addEventListener('blur', validateField);
+
+    // Send Email & reset button
+    resetBtn.addEventListener('click', resetForm);
 }
 
 
@@ -47,8 +52,9 @@ function validateField() {
 
     // Check that the inputs are not empty
     if(email.value !== '' && subject.value !== '' && message.value !== '') {
-        if(errors.length) {
-            
+        if(errors.length === 0) {
+            // the button should be enabled
+            sendBtn.disabled = false;
         }
     }
 }
@@ -76,4 +82,9 @@ function validateEmail(field) {
         field.style.borderBottomColor = 'red';
         field.classList.add('error')
     }
+}
+
+// Reset the form
+function resetForm() {
+    sendEmailForm.reset();
 }
